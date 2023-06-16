2124a310a8111dd831d827ca5018576d0655e303
Aleksey <wallentaine@gmail.com>
2023-06-17 01:29:00 +0300

    feat: add sass to project

--- frontend/components/track/TrackItem.tsx
+++ frontend/components/track/TrackItem.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { ITrack } from '../../types/track.types';
-import { Card } from '@mui/material';
+import { Card, Grid, IconButton } from '@mui/material';
+import styles from '../../styles/TrackItem.module.scss';
+import { Delete, Pause, PlayArrow } from '@mui/icons-material';
 
 interface TrackItemProps {
     track: ITrack;
     active?: boolean;
 }
 
-const TrackItem: React.FC<TrackItemProps> = ({ track, active = false }) => {
+const TrackItem: React.FC<TrackItemProps> = ({ track, active = true }) => {
     return (
-        <Card sx={{padding: 2, marginBottom: 2}}>
-            {track.name}
+        <Card className={styles.track}>
+            <img width={48} height={48} src={track.picture} alt='' />
+            <IconButton sx={{ marginX: 2 }}>
+                {active ?
+                    <Pause />
+                    :
+                    <PlayArrow />
+                }
+            </IconButton>
+            <Grid container direction='column' style={{ width: 200 }}>
+                <div>{track.name}</div>
+                <div style={{ fontSize: 12, color: 'gray' }}>{track.artist}</div>
+            </Grid>
+            {active && <div>02:42 / 03:43</div>}
+            <IconButton style={{ marginLeft: 'auto' }}>
+                <Delete />
+            </IconButton>
         </Card>
     );
 };
